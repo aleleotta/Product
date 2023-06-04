5e979d5cb765d02ad614fa2a2f795b501368a801
Alessandro Leotta <Alessandro Leotta@HPaleleotta>
2023-06-04 03:26:06 +0200

    .

---
+++ Product/src/exercise/Main.java
@@ -0,0 +1,62 @@
+package exercise;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		LinkedHashSet<Product> list = new LinkedHashSet<Product>();
+		Scanner sc = new Scanner(System.in);
+		Product obj;
+		String name = "";
+		double price = 0;
+		int expireDays = 0;
+		String type = "";
+		int option = 10;
+		while(option != 0) {
+			Product.menu();
+			option = sc.nextInt();
+			sc.nextLine();
+			switch(option) {
+			case 1: //INSERT
+				char option1 = 'z';
+				System.out.print("Introduce the product name: ");
+				name = sc.nextLine();
+				System.out.print("Introduce the product price: ");
+				price = sc.nextDouble();
+				sc.nextLine();
+				System.out.println("Is the following product expirable?");
+				while(!(option1 == 'Y' || option1 == 'y' || option1 == 'N' || option1 == 'n')){
+					option1 = sc.nextLine().charAt(0);
+					if(option1 == 'Y' || option1 == 'y') {
+						System.out.print("Introduce expiration days: ");
+						expireDays = sc.nextInt();
+						sc.nextLine();
+						obj = new Expirable(name, price, expireDays);
+						list.add(obj);
+					} else if(option1 == 'N' || option1 == 'n') {
+						System.out.print("Introduce the type of product: ");
+						type = sc.nextLine();
+						obj = new NotExpirable(name, price, type);
+						list.add(obj);
+					}
+				}
+				break;
+			case 2: //READ
+				for(Product instance: list) {
+					System.out.println(instance.toString());
+					System.out.println();
+				}
+				break;
+			case 3: //DELETE
+				list.remove();
+				break;
+			case 0: //EXIT
+				System.out.println("Exiting program...");
+				break;
+			default: //ERROR
+				System.out.println("Please introduce a valid option.");
+				break;
+			}
+		}
+		sc.close();
+	}
+}
--- Product/src/exercise/Product.java
+++ Product/src/exercise/Product.java
@@ -59,4 +59,16 @@ public class Product implements Comparable<Product> {
 		}
 		return check;
 	}
+	
+	public static void menu() {
+		System.out.println("MENU");
+		System.out.println("====================================================");
+		System.out.println("1) Add product");
+		System.out.println("2) Print list of products");
+		System.out.println("3) Delete product");
+		System.out.println("0) Exit");
+		System.out.println();
+		System.out.print("Option: ");
+	}
+	
 }
